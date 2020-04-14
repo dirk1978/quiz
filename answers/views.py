53b8b2e68560ab2bd98ce1193eef12955d7f099e
Darren Mansell <Darren.Mansell@redland-solutions.co.uk>
2020-04-14 18:22:16 +0100

    urls

--- answers/views.py
+++ answers/views.py
@@ -9,64 +9,64 @@ def index(request):
         form = ResultForm(request.POST)
         if form.is_valid():
             obj = form.save(commit=False)
-            if fuzz.ratio(obj.question_1,obj.this_round.answer_1) > 80:
+            if fuzz.ratio(obj.question_1, obj.this_round.answer_1) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_1
-            if obj.question_2 == obj.this_round.answer_2:
+            if fuzz.ratio(obj.question_2, obj.this_round.answer_2) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_2
-            if obj.question_3 == obj.this_round.answer_3:
+            if fuzz.ratio(obj.question_3, obj.this_round.answer_3) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_3
-            if obj.question_4 == obj.this_round.answer_4:
+            if fuzz.ratio(obj.question_4, obj.this_round.answer_4) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_4
-            if obj.question_5 == obj.this_round.answer_5:
+            if fuzz.ratio(obj.question_5, obj.this_round.answer_5) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_5
-            if obj.question_6 == obj.this_round.answer_6:
+            if fuzz.ratio(obj.question_6, obj.this_round.answer_6) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_6
-            if obj.question_7 == obj.this_round.answer_7:
+            if fuzz.ratio(obj.question_7, obj.this_round.answer_7) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_7
-            if obj.question_8 == obj.this_round.answer_8:
+            if fuzz.ratio(obj.question_8, obj.this_round.answer_8) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_8
-            if obj.question_9 == obj.this_round.answer_9:
+            if fuzz.ratio(obj.question_9, obj.this_round.answer_9) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_9
-            if obj.question_10 == obj.this_round.answer_10:
+            if fuzz.ratio(obj.question_10, obj.this_round.answer_10) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_10
-            if obj.question_11 == obj.this_round.answer_11:
+            if fuzz.ratio(obj.question_11, obj.this_round.answer_11) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_11
-            if obj.question_12 == obj.this_round.answer_12:
+            if fuzz.ratio(obj.question_12, obj.this_round.answer_12) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_12
-            if obj.question_13 == obj.this_round.answer_13:
+            if fuzz.ratio(obj.question_13, obj.this_round.answer_13) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_13
-            if obj.question_14 == obj.this_round.answer_14:
+            if fuzz.ratio(obj.question_14, obj.this_round.answer_14) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_14
-            if obj.question_15 == obj.this_round.answer_15:
+            if fuzz.ratio(obj.question_15, obj.this_round.answer_15) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_15
-            if obj.question_16 == obj.this_round.answer_16:
+            if fuzz.ratio(obj.question_16, obj.this_round.answer_16) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_16
-            if obj.question_17 == obj.this_round.answer_17:
+            if fuzz.ratio(obj.question_17, obj.this_round.answer_17) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_17
-            if obj.question_18 == obj.this_round.answer_18:
+            if fuzz.ratio(obj.question_18, obj.this_round.answer_18) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_18
-            if obj.question_19 == obj.this_round.answer_19:
+            if fuzz.ratio(obj.question_19, obj.this_round.answer_19) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_19
-            if obj.question_20 == obj.this_round.answer_20:
+            if fuzz.ratio(obj.question_20, obj.this_round.answer_20) > 80:
                 if obj.score == None: obj.score = 0
                 obj.score += obj.this_round.score_20
             obj.save()
